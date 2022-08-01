cc8f0b15cf4d627d02e27c52989fa53c10e86870
liangxiong <1071608617@qq.com>
2022-08-01 16:22:23 +0800

    add ReentrantReadWriteLock demo

--- src/top/xiangqian/concurrency/beautiful/chapter6/aqs/ReentrantReadWriteLockListTest.java
+++ src/top/xiangqian/concurrency/beautiful/chapter6/aqs/ReentrantReadWriteLockListTest.java
@@ -8,17 +8,30 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * @author xiangqian
- * @date 2022/7/31
- * @description:
+ * @date 2022/8/1
+ * @description: ReentrantReadWriteLock实例代码
  **/
 public class ReentrantReadWriteLockListTest {
 
-    private static final List<String> NUMBERS = new ArrayList<>(2);
+    private static final List<String> NUMBERS = new ArrayList<>(10);
 
+    /**
+     * static final int SHARED_SHIFT   = 16;
+     * static final int SHARED_UNIT    = (1 << SHARED_SHIFT) = 65536;
+     * static final int MAX_COUNT      = (1 << SHARED_SHIFT) - 1 = 65535;
+     * AQS状态值state的高16位: 表示获取读锁的线程个数,最大值: SHARED_UNIT = 65536
+     * AQS状态值state的低16位: 表示获取到写锁的线程可重入次数,最大值: MAX_COUNT = (1 << SHARED_SHIFT) - 1 = 65535;
+     */
     private static final ReadWriteLock LOCK = new ReentrantReadWriteLock();
 
+    /**
+     * 读锁是共享锁(当前如果有其他线程获取写锁,则当前线程无法获取读锁)
+     */
     private static final Lock READ_LOCK = LOCK.readLock();
 
+    /**
+     * 写锁是排他锁,可重入(当前如果有其他线程获取了读锁或写锁,则当前线程无法获取写锁)
+     */
     private static final Lock WRITE_LOCK = LOCK.writeLock();
 
     private static void add(String num) {
